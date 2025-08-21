be0accb793870e2bfbc1524b2dd9a56542a911e0
MaksimNemoshenkov <shalun084@gmail.com>
2025-08-21 20:36:00 +0200

    fix serverUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: "https://backendaplication-8a88e673b0bc.herokuapp.com/"
+  serverUrl: "https://backendaplication-8a88e673b0bc.herokuapp.com"
 };
